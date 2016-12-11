4e50388de2ee3e028fc8afd16f36d9cb49728cf2
B9 <B9@B9-PC>
2016-12-11 16:57:23 +0600

    add dao and service package

---
+++ src/main/java/com/marmansys/controller/AppConfig.java
@@ -0,0 +1,16 @@
+
+package com.marmansys.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/")
+public class AppConfig {
+    @RequestMapping(method = RequestMethod.GET)
+	    public String getIndexPage() {
+	        return "index";
+	    }
+}
