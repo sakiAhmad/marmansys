1755bcd0959372ff693d5b20358d1e8c09122604
aSaki <playerstored@gmail.com>
2016-12-12 00:26:57 +0600

    simplify dao classes

--- src/main/java/com/marmansys/dao/CompanyDao.java
+++ src/main/java/com/marmansys/dao/CompanyDao.java
@@ -5,11 +5,4 @@ import com.marmansys.entity.Company;
 import java.util.List;
 
 public interface CompanyDao {
-   
-
-    void save(Company company);
-
-    void deleteBySSO(String sso);
-
-    List<Company> findAllUsers();
 }
--- src/main/java/com/marmansys/dao/CompanyDaoImpl.java
+++ src/main/java/com/marmansys/dao/CompanyDaoImpl.java
@@ -12,35 +12,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository("companyDao")
 public class CompanyDaoImpl extends AbstractDao implements CompanyDao{
- 
-    @Override
-      public void save(Company company) {
-        persist(company);
-    }
-
-    @Override
-    public void deleteBySSO(String sso) {
-        Criteria crit = createEntityCriteria();
-        crit.add(Restrictions.eq("ssoId", sso));
-        Company company = (Company) crit.uniqueResult();
-        delete(company);
-    }
-
-   @SuppressWarnings("unchecked")
-    public List<Company> findAllUsers() {
-        Criteria criteria = createEntityCriteria().addOrder(Order.asc("firstName"));
-        criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);//To avoid duplicates.
-        List<Company> users = (List<Company>) criteria.list();
-
-		// No need to fetch userProfiles since we are not showing them on list page. Let them lazy load. 
-        // Uncomment below lines for eagerly fetching of userProfiles if you want.
-		/*
-         for(User user : users){
-         Hibernate.initialize(user.getUserProfiles());
-         }*/
-        return users;
-    }
-
-    
     
 }
