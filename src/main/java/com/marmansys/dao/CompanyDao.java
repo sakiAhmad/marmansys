54809b216f9c2236f643a5ccc3b6d4fa2f8e902b
Ridwan <Ridwan@B5-PC>
2016-12-11 17:12:01 +0600

    ridwan--abstractdao

--- src/main/java/com/marmansys/dao/CompanyDao.java
+++ src/main/java/com/marmansys/dao/CompanyDao.java
@@ -1,6 +1,17 @@
 
 package com.marmansys.dao;
 
+import com.marmansys.entity.Company;
+import java.util.List;
+
 public interface CompanyDao {
-    
+    Company findById(int id);
+
+    Company findBySSO(String sso);
+
+    void save(Company company);
+
+    void deleteBySSO(String sso);
+
+    List<Company> findAllUsers();
 }
