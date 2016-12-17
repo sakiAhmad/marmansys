7f09ab714def8239840f8e787adbd2bcfe9430bf
aSaki <playerstored@gmail.com>
2016-12-17 14:01:56 +0600

    simplify dao classes

--- src/main/java/com/marmansys/controller/AdminController.java
+++ src/main/java/com/marmansys/controller/AdminController.java
@@ -8,30 +8,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-    
-    @Autowired
-    CompanyService companyService;
 
     @RequestMapping(method = RequestMethod.GET)
     public String adminPanel() {
         return "admin/admin-dashbord";
     }
     
-    @RequestMapping(value="/insert-company" , method = RequestMethod.GET)
-    public String addCompany() {
-        return "admin/insert-company";
+    @RequestMapping(value="/company" , method = RequestMethod.GET)
+    public String manageCompany() {
+        return "admin/company";
     }
-
-//    @RequestMapping(value = "/admin/add-company", method = RequestMethod.GET)
-//    public String addCompany() {
-//        Company company = new Company();
-//
-//        company.setCompanyName("Love");
-//        companyService.addCompany(company);
-//        return "index";
-//    }
 }
