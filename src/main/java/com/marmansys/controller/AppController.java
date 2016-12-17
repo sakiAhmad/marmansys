4690cafb0d8e021c2b8ce0ed34be11064a0129c0
B9 <B9@B9-PC>
2016-12-17 16:49:08 +0600

    some modification..

--- src/main/java/com/marmansys/controller/AppController.java
+++ src/main/java/com/marmansys/controller/AppController.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class AppController {
-
     
-
     @RequestMapping(method = RequestMethod.GET)
     public String getIndexPage() {
         return "index";
--- src/main/java/com/marmansys/controller/RESTfulController.java
+++ src/main/java/com/marmansys/controller/RESTfulController.java
@@ -1,9 +1,18 @@
-
 package com.marmansys.controller;
 
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RESTfulController {
 
+    @RequestMapping(value = "admin/company/test", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String listAllUsers() {
+        return "Hello Rest";
+    }
 }
